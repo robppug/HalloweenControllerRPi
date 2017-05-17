33da3e9d1beb4086948b394e85566355774501d9
Roberto.Pugliese <n13astra@hotmail.com>
2017-05-17 11:47:53 +1000

    - Changed: SOUND Hat to support 2 SOUND drivers.

--- HalloweenControllerRPi/Device/Controllers/RaspberryPi/Hats/RPiHat_SOUND_v1.cs
+++ HalloweenControllerRPi/Device/Controllers/RaspberryPi/Hats/RPiHat_SOUND_v1.cs
@@ -12,14 +12,14 @@ namespace HalloweenControllerRPi.Device.Controllers.RaspberryPi.Hats
 {
    public class RPiHat_SOUND_v1 : RPiHat
    {
-      IDriverSoundProvider soundDriver;
+      List<IDriverSoundProvider> soundDrivers;
       BusDevice_SC16IS752 busDevice;
       public UInt16 address;
 
       public RPiHat_SOUND_v1(IHWController host, I2cDevice i2cDevice, UInt16 hatAddress) : base(host)
       {
          HatType = SupportedHATs.SOUND_v1;
-         soundDriver = new Catalex_YX5300();
+         soundDrivers = new List<IDriverSoundProvider>((int)busDevice.NumberOfChannels);
          busDevice = new BusDevice_SC16IS752();
          address = hatAddress;
 
@@ -29,12 +29,12 @@ namespace HalloweenControllerRPi.Device.Controllers.RaspberryPi.Hats
          /* Initialise the BUS DEVICE */
          busDevice.InitialiseChannels();
 
-         soundDriver.InitialiseDriver();
+         InitialiseSoundDrivers();
 
          Channels = new List<IChannel>();
 
          /* Initialise availble channels on attached HAT */
-         for (uint i = 0; i < 2; i++)
+         for (uint i = 0; i < soundDrivers.Count; i++)
          {
             ChannelFunction_SOUND chan = null;
 
@@ -47,17 +47,20 @@ namespace HalloweenControllerRPi.Device.Controllers.RaspberryPi.Hats
          }
       }
 
-      private void InitialiseDriver()
+      private void InitialiseSoundDrivers()
       {
          List<byte> data = new List<byte>();
 
-         soundDriver.InitialiseDriver();
+         foreach (IDriverSoundProvider snd in soundDrivers)
+         {
+            snd.InitialiseDriver();
 
-         (soundDriver as Catalex_YX5300).BuildCommand(ref data, Catalex_YX5300.COMMANDS.SEL_DEV, 0x02);
+            (snd as Catalex_YX5300).BuildCommand(ref data, Catalex_YX5300.COMMANDS.SEL_DEV, 0x02);
 
-         //busDevice.WriteBytes(0, data);
+            //busDevice.WriteBytes(0, data);
 
-         Task.Delay(500);
+            Task.Delay(500);
+         }
       }
 
       public override void RefreshChannel(IChannel chan)
